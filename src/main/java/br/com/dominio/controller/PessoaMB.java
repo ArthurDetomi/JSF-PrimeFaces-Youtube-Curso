97d7ac3961c2a5f68e5f1b69c178aa263436aa0f
Arthur Detomi <arthurdetomi@gmail.com>
2023-01-24 17:04:05 -0300

    Primeiros passos com JSF e PrimeFaces

---
+++ src/main/java/br/com/dominio/controller/PessoaMB.java
@@ -0,0 +1,54 @@
+package br.com.dominio.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.dominio.model.Pessoa;
+
+@Named("bean")
+@SessionScoped
+public class PessoaMB implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private Pessoa pessoa;
+	
+	private List<Pessoa> pessoas = new ArrayList<>();
+	
+	public String adicionar() {
+		pessoas.add(pessoa);
+		limpar();
+		return null;
+	}
+	
+	public void limpar() {
+		pessoa = new Pessoa();
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/br/com/dominio/model/Pessoa.java
@@ -0,0 +1,58 @@
+package br.com.dominio.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Pessoa implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	private String nome;
+	
+	private String email;
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
