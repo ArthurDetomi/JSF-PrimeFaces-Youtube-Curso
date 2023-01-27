586f88cf82b47b42c7bb4720cce6b511cbe28f56
Arthur Detomi <arthurdetomi@gmail.com>
2023-01-26 21:43:25 -0300

    Modelo Medicamento #42

---
+++ src/main/java/br/com/medicamento/model/Medicamento.java
@@ -0,0 +1,85 @@
+package br.com.medicamento.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Medicamento implements Serializable, Base {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+
+	private String nome;
+
+	private String apresentacao;
+
+	private String laboratorio;
+
+	private String registro;
+
+	private BigDecimal preco;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Medicamento other = (Medicamento) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getApresentacao() {
+		return apresentacao;
+	}
+
+	public void setApresentacao(String apresentacao) {
+		this.apresentacao = apresentacao;
+	}
+
+	public String getLaboratorio() {
+		return laboratorio;
+	}
+
+	public void setLaboratorio(String laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+
+	public String getRegistro() {
+		return registro;
+	}
+
+	public void setRegistro(String registro) {
+		this.registro = registro;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+}
