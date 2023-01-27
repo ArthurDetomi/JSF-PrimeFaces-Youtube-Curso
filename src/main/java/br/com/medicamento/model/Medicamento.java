fe35a6572b079f0e52064505aa880bcc1fc5a419
Arthur Detomi <arthurdetomi@gmail.com>
2023-01-26 21:52:41 -0300

    Mapeando entidade Medicamento #43

--- src/main/java/br/com/medicamento/model/Medicamento.java
+++ src/main/java/br/com/medicamento/model/Medicamento.java
@@ -4,10 +4,20 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "medicamentos")
 public class Medicamento implements Serializable, Base {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String nome;
